23aac9f49d2637fb5bf70ff4bf4dee3d200a4198
Mehmet Nuraydın <meanllbrl@gmail.com>
2025-01-15 10:14:36 +0300

    update

--- lib/src/positioned_list.dart
+++ lib/src/positioned_list.dart
@@ -167,71 +167,75 @@ class _PositionedListState extends State<PositionedList> {
   @override
   Widget build(BuildContext context) => RegistryWidget(
         elementNotifier: registeredElements,
-        child: UnboundedCustomScrollView(
-          anchor: widget.alignment,
-          center: _centerKey,
+        child: PrimaryScrollController(
           controller: scrollController,
-          scrollDirection: widget.scrollDirection,
-          reverse: widget.reverse,
-          cacheExtent: widget.cacheExtent,
-          physics: widget.physics,
-          shrinkWrap: widget.shrinkWrap,
-          semanticChildCount: widget.semanticChildCount ?? widget.itemCount,
-          slivers: <Widget>[
-            if (widget.positionedIndex > 0)
+      
+          child: UnboundedCustomScrollView(
+            anchor: widget.alignment,
+            center: _centerKey,
+            controller: scrollController,
+            scrollDirection: widget.scrollDirection,
+            reverse: widget.reverse,
+            cacheExtent: widget.cacheExtent,
+            physics: widget.physics,
+            shrinkWrap: widget.shrinkWrap,
+            semanticChildCount: widget.semanticChildCount ?? widget.itemCount,
+            slivers: <Widget>[
+              if (widget.positionedIndex > 0)
+                SliverPadding(
+                  padding: _leadingSliverPadding,
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) => widget.separatorBuilder == null
+                          ? _buildItem(widget.positionedIndex - (index + 1))
+                          : _buildSeparatedListElement(
+                              2 * widget.positionedIndex - (index + 1)),
+                      childCount: widget.separatorBuilder == null
+                          ? widget.positionedIndex
+                          : 2 * widget.positionedIndex,
+                      addSemanticIndexes: false,
+                      addRepaintBoundaries: widget.addRepaintBoundaries,
+                      addAutomaticKeepAlives: widget.addAutomaticKeepAlives,
+                    ),
+                  ),
+                ),
               SliverPadding(
-                padding: _leadingSliverPadding,
+                key: _centerKey,
+                padding: _centerSliverPadding,
                 sliver: SliverList(
                   delegate: SliverChildBuilderDelegate(
                     (context, index) => widget.separatorBuilder == null
-                        ? _buildItem(widget.positionedIndex - (index + 1))
+                        ? _buildItem(index + widget.positionedIndex)
                         : _buildSeparatedListElement(
-                            2 * widget.positionedIndex - (index + 1)),
-                    childCount: widget.separatorBuilder == null
-                        ? widget.positionedIndex
-                        : 2 * widget.positionedIndex,
+                            index + 2 * widget.positionedIndex),
+                    childCount: widget.itemCount != 0 ? 1 : 0,
                     addSemanticIndexes: false,
                     addRepaintBoundaries: widget.addRepaintBoundaries,
                     addAutomaticKeepAlives: widget.addAutomaticKeepAlives,
                   ),
                 ),
               ),
-            SliverPadding(
-              key: _centerKey,
-              padding: _centerSliverPadding,
-              sliver: SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) => widget.separatorBuilder == null
-                      ? _buildItem(index + widget.positionedIndex)
-                      : _buildSeparatedListElement(
-                          index + 2 * widget.positionedIndex),
-                  childCount: widget.itemCount != 0 ? 1 : 0,
-                  addSemanticIndexes: false,
-                  addRepaintBoundaries: widget.addRepaintBoundaries,
-                  addAutomaticKeepAlives: widget.addAutomaticKeepAlives,
-                ),
-              ),
-            ),
-            if (widget.positionedIndex >= 0 &&
-                widget.positionedIndex < widget.itemCount - 1)
-              SliverPadding(
-                padding: _trailingSliverPadding,
-                sliver: SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) => widget.separatorBuilder == null
-                        ? _buildItem(index + widget.positionedIndex + 1)
-                        : _buildSeparatedListElement(
-                            index + 2 * widget.positionedIndex + 1),
-                    childCount: widget.separatorBuilder == null
-                        ? widget.itemCount - widget.positionedIndex - 1
-                        : 2 * (widget.itemCount - widget.positionedIndex - 1),
-                    addSemanticIndexes: false,
-                    addRepaintBoundaries: widget.addRepaintBoundaries,
-                    addAutomaticKeepAlives: widget.addAutomaticKeepAlives,
+              if (widget.positionedIndex >= 0 &&
+                  widget.positionedIndex < widget.itemCount - 1)
+                SliverPadding(
+                  padding: _trailingSliverPadding,
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) => widget.separatorBuilder == null
+                          ? _buildItem(index + widget.positionedIndex + 1)
+                          : _buildSeparatedListElement(
+                              index + 2 * widget.positionedIndex + 1),
+                      childCount: widget.separatorBuilder == null
+                          ? widget.itemCount - widget.positionedIndex - 1
+                          : 2 * (widget.itemCount - widget.positionedIndex - 1),
+                      addSemanticIndexes: false,
+                      addRepaintBoundaries: widget.addRepaintBoundaries,
+                      addAutomaticKeepAlives: widget.addAutomaticKeepAlives,
+                    ),
                   ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       );
 
